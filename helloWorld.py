a1d2579cb52d4b508707a545cc5d02a59078f471
Shraddha N <shraddhan654@gmail.com>
2021-10-30 21:20:59 -0400

    Added hello world file

---
+++ helloWorld.py
@@ -0,0 +1,16 @@
+def sum(a, b, c=0, d=None, e=0):
+    print(f"a: {a}, b: {b}, c: {c}, d: {d}, e: {e}")
+    total = a + b + c + e
+    if d is not None:
+        total += d
+    return total
+
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# print(sum(a=x, c=z, e=y, b=None, d=None))
+
+print(sum(1, 2))
+print(sum(1, 2, 3))
+print(sum(1, 2, 3, 4))
+print(sum(1, 2, 3, 4, 5))
